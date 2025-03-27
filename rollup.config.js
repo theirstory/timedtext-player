5e3db8919422f84c2bbc0dcb23e380d61a19bd13
Laurian Gridinoc <laurian@gridinoc.name>
2025-03-27 16:11:09 +0200

    source maps changes

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,6 @@
  */
 
 import summary from 'rollup-plugin-summary';
-import {terser} from 'rollup-plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 
@@ -14,31 +13,14 @@ export default {
   output: {
     file: 'dist/timedtext-player.bundled.js',
     format: 'esm',
+    sourcemap: 'inline',
   },
   onwarn(warning) {
     if (warning.code !== 'THIS_IS_UNDEFINED') {
       console.error(`(!) ${warning.message}`);
     }
   },
-  plugins: [
-    replace({'Reflect.decorate': 'undefined'}),
-    resolve(),
-    /**
-     * This minification setup serves the static site generation.
-     * For bundling and minification, check the README.md file.
-     */
-    terser({
-      ecma: 2021,
-      module: true,
-      warnings: true,
-      mangle: {
-        properties: {
-          regex: /^__/,
-        },
-      },
-    }),
-    summary(),
-  ],
+  plugins: [replace({ 'Reflect.decorate': 'undefined' }), resolve(), summary()],
 };
 
 // TODO: sourcemaps + d.ts? https://gist.github.com/rikkit/b636076740dfaa864ce9ee8ae389b81c
