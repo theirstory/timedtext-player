b70144faad527d7a175e9fd4757d13a6fe2ef411
Laurian Gridinoc <laurian@gridinoc.name>
2025-05-21 14:54:27 +0300

    timecode fix

--- rollup.config.js
+++ rollup.config.js
@@ -7,6 +7,7 @@
 import summary from 'rollup-plugin-summary';
 import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
+import commonjs from '@rollup/plugin-commonjs'; // <-- add this line
 
 export default {
   input: 'dist/timedtext-player.js',
@@ -15,12 +16,18 @@ export default {
     format: 'esm',
     sourcemap: 'inline',
   },
+  external: [], // <--- Add this line!
   onwarn(warning) {
     if (warning.code !== 'THIS_IS_UNDEFINED') {
       console.error(`(!) ${warning.message}`);
     }
   },
-  plugins: [replace({ 'Reflect.decorate': 'undefined' }), resolve(), summary()],
+  plugins: [
+    replace({ 'Reflect.decorate': 'undefined' }),
+    resolve(),
+    commonjs({ defaultIsModuleExports: true }), // <-- add this line
+    summary(),
+  ],
 };
 
 // TODO: sourcemaps + d.ts? https://gist.github.com/rikkit/b636076740dfaa864ce9ee8ae389b81c
